80313fc9552eac8f6be4ddc60ec40619100d52ea
Venkat <gvenkata1994@gmail.com>
2021-12-16 20:27:48 +0530

    Tensorflow code to run on M1 Pro

--- tf-coreml-conversion.py
+++ tf-coreml-conversion.py
@@ -9,12 +9,8 @@ if __name__=="__main__":
         
     # Load the saved tf model first
     tf_model = tf.keras.models.load_model('saved_model3/')
-    
-    # Now create a new model with Softmax on top
-    sftmax = tf.keras.layers.Softmax(axis=-1)
-    ml_model = tf.keras.models.Model(tf_model.input, sftmax(tf_model.layers[-1].output))
 
-    ct_model = ct.convert(ml_model, inputs=[ct.ImageType()], 
+    ct_model = ct.convert(tf_model, inputs=[ct.ImageType()], 
                         classifier_config = ct.ClassifierConfig(class_labels))
                         
     spec = ct_model.get_spec()
@@ -35,20 +31,10 @@ if __name__=="__main__":
     ctmodel.license = "MIT License"
 
     # Set a short description for the Xcode UI
-    ctmodel.short_description = "Detects the most likely LaTeX mathematical symbol \
-                               corresponding to a drawing."
+    ctmodel.short_description = "Detects the most likely LaTeX mathematical symbol corresponding to a drawing."
 
     # Set a version for the model
-    ctmodel.version = "0.96"
+    ctmodel.version = "1.0"
 
     # Save model
     ctmodel.save("deTeXtf3.mlmodel")
-
-    # Quantisation to FP16 model that reduces size by half without (supposedly)
-    # affecting accuracy
-
-#     ctmodel_fp16 = quantization_utils.quantize_weights(ctmodel, nbits=16)
-#     ctmodel_fp16.save("deTeX16.mlmodel")
-# 
-#     ctmodel_fp8 = quantization_utils.quantize_weights(ctmodel, nbits=8)
-#     ctmodel_fp8.save("deTeX8.mlmodel")
--- tf-train-detex.py
+++ tf-train-detex.py
@@ -1,18 +1,13 @@
 import tensorflow as tf
 import pdb
 
-EPOCHS = 1
-BATCH_SIZE = 64
+EPOCHS = 5
+BATCH_SIZE = 128
 LEARNING_RATE = 0.003
-SIGDIG=3
 SEED=1220
-THRESHOLD=0.001
 
-if __name__ == '__main__':
-	
-	#Set random seeds
-	
-	# Load the Data
+if __name__ == '__main__':	
+	# Load train and validation data
 	train_ds = tf.keras.preprocessing.image_dataset_from_directory(
 								'/Volumes/detext/drawings/',
 							    color_mode="grayscale",
@@ -20,26 +15,39 @@ if __name__ == '__main__':
   								batch_size=BATCH_SIZE,
 								labels='inferred',
 								label_mode='int',
-								image_size=(200,300))
-
+								image_size=(200,300),
+								validation_split=0.1,
+								subset='training')
+	
+	val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+								'/Volumes/detext/drawings/',
+							    color_mode="grayscale",
+  								seed=SEED,
+  								batch_size=BATCH_SIZE,
+								labels='inferred',
+								label_mode='int',
+								image_size=(200,300),
+								validation_split=0.1,
+								subset='validation')
+	
 	# Get the class names
-    # class_ids = {v:k for k,v in data.class_to_idx.items()}
 	class_names = train_ds.class_names
 	num_classes = len(class_names)
 
-	# Create model - MNetV3 automatically does rescaling and normalization automatically
+	# Create model
 	model = tf.keras.applications.MobileNetV3Small(
     	input_shape=(200,300,1), alpha=1.0, minimalistic=False, 
     	include_top=True, weights=None, input_tensor=None, classes=num_classes,
-    	pooling=None, dropout_rate=0, classifier_activation=None)
-	
+    	pooling=None, classifier_activation="softmax",
+    	include_preprocessing=True)
+
 	# Compile model
-	model.compile(optimizer='adam',
-           loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-           metrics=['accuracy'])
-# 	model.summary()	
+	model.compile(
+		   optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
+           loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+           metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
     # Training
-	model.fit(train_ds, epochs=EPOCHS)
-	
+	model.fit(train_ds, epochs=EPOCHS, validation_data=val_ds)
+
 	model.save('./saved_model3/')
